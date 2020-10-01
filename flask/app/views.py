80b0710d76c5149787a761752e31e40a019ef32b
Jaime Sendra <jsendra@autis.es>
2020-10-01 23:54:15 +0200

    refactory

---
+++ flask/app/views.py
@@ -0,0 +1,41 @@
+# from typing import List, Dict
+# import mysql.connector
+# import json
+
+# from app import app
+
+# def favorite_colors() -> List[Dict]:
+#     config = {
+#         'user': 'root',
+#         'password': 'root',
+#         'host': 'db',
+#         'port': '3306',
+#         'database': 'knights'
+#     }
+#     connection = mysql.connector.connect(**config)
+#     cursor = connection.cursor()
+#     cursor.execute('SELECT * FROM favorite_colors')
+#     results = [{name: color} for (name, color) in cursor]
+#     cursor.close()
+#     connection.close()
+
+#     return results
+
+
+# @app.route('/')
+# def index() -> str:
+#     return json.dumps({'favorite_colors': favorite_colors()})
+
+from app import app
+import os
+
+@app.route("/")
+def index():
+
+    # Use os.getenv("key") to get environment variables
+    app_name = os.getenv("APP_NAME")
+
+    if app_name:
+        return f"Hello from {app_name} running in a Docker container behind Nginx!"
+
+    return "Hello from Flask"
