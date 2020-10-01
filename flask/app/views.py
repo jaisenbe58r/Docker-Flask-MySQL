6be3012e9c3a878f26c5736042ebe88cb63ae76c
Jaime Sendra <jsendra@autis.es>
2020-10-02 00:13:17 +0200

    prueba

--- flask/app/views.py
+++ flask/app/views.py
@@ -5,22 +5,22 @@ import os
 
 from app import app
 
-# def favorite_colors() -> List[Dict]:
-#     config = {
-#         'user': 'root',
-#         'password': 'root',
-#         'host': 'db',
-#         'port': '3306',
-#         'database': 'knights'
-#     }
-#     connection = mysql.connector.connect(**config)
-#     cursor = connection.cursor()
-#     cursor.execute('SELECT * FROM favorite_colors')
-#     results = [{name: color} for (name, color) in cursor]
-#     cursor.close()
-#     connection.close()
+def favorite_colors() -> List[Dict]:
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'knights'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM favorite_colors')
+    results = [{name: color} for (name, color) in cursor]
+    cursor.close()
+    connection.close()
 
-#     return results
+    return results
 
 
 @app.route("/")
@@ -36,6 +36,6 @@ def index():
 
 
 
-# @app.route('/db')
-# def index() -> str:
-#     return json.dumps({'favorite_colors': favorite_colors()})
+@app.route('/db')
+def index() -> str:
+    return json.dumps({'favorite_colors': favorite_colors()})
